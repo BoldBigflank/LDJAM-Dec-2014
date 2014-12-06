28a2a6d47842323b3061ca4ab758c1c08cecd8aa
Alex Swan <alex@bold-it.com>
2014-12-06 00:01:18 -0800

    initial

---
+++ Assets/PlayerInputScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputScript : MonoBehaviour {
+	float baseSpeed = 15.0F;
+	float inputDeadSquare = 0.13F * 0.13F;
+	
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter (Collision collision){
+		Debug.Log("Player Collision Enter");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		float xAxis = Input.GetAxis ("Horizontal");
+		float zAxis = Input.GetAxis ("Vertical");
+		Vector3 direction = Vector3.zero;
+		if(xAxis * xAxis + zAxis * zAxis > inputDeadSquare){
+			direction.x += xAxis * baseSpeed * Time.deltaTime;
+			direction.z += zAxis * baseSpeed * Time.deltaTime;
+			transform.rotation = Quaternion.LookRotation(direction);
+		}
+		transform.position += direction;
+	
+	}
+}
